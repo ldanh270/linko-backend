2e2f189939be00c3b38edda37a8793ef8615db08
ldanh270 <ducanhle.dn@gmail.com>
2025-12-14 19:54:08 +0700

    feat(models): add message model

---
+++ src/models/Message.ts
@@ -0,0 +1,35 @@
+import mongoose, { InferSchemaType } from "mongoose"
+
+const messageSchema = new mongoose.Schema(
+    {
+        conversationId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Conversation",
+            required: true,
+            index: true,
+        },
+
+        senderId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+            index: true,
+        },
+        content: {
+            type: String,
+            trim: true,
+        },
+    },
+    {
+        // Auto create createdAt & updatedAt
+        timestamps: true,
+    },
+)
+
+messageSchema.index({ conversationId: 1, createdAt: -1 })
+
+const Message = mongoose.model("Message", messageSchema)
+
+export default Message
+
+export type MessageType = InferSchemaType<typeof messageSchema>
