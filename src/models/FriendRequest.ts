b4a5644827d1b2d62067f8157d5119b2a6833a6e
ldanh270 <ducanhle.dn@gmail.com>
2025-12-15 18:17:44 +0700

    feat(model): add FriendRequest

---
+++ src/models/FriendRequest.ts
@@ -0,0 +1,40 @@
+import mongoose, { InferSchemaType } from "mongoose"
+import { maxLength } from "zod"
+
+const friendRequestSchema = new mongoose.Schema(
+    {
+        from: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        to: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        message: {
+            type: String,
+            maxLength: 300,
+        },
+    },
+    {
+        // Auto create createdAt & updatedAt
+        timestamps: true,
+    },
+)
+
+// To avoid duplicate requests
+friendRequestSchema.index({ from: 1, to: 1 }, { unique: true })
+
+// To get sent requests
+friendRequestSchema.index({ from: 1 })
+
+// To get recieved requests
+friendRequestSchema.index({ to: 1 })
+
+const FriendRequest = mongoose.model("FriendRequest", friendRequestSchema)
+
+export default FriendRequest
+
+export type FriendRequestType = InferSchemaType<typeof friendRequestSchema>
