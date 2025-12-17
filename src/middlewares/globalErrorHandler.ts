9c564dbbea9cd10c81cd77b94c689e2e854af23e
ldanh270 <ducanhle.dn@gmail.com>
2025-12-17 21:56:33 +0700

    feat(error): add global handle error middleware & AppError

---
+++ src/middlewares/globalErrorHandler.ts
@@ -0,0 +1,47 @@
+import { NextFunction, Request, Response } from "express"
+
+// Format errors for Developer: Detailed stack trace to fix bug
+const sendErrorDev = (err: any, res: Response) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack,
+    })
+}
+
+// Format errors for Client(Production): Thin, clean to hide logic
+const sendErrorClient = (err: any, res: Response) => {
+    // Handled errors (Operational) => Send detailed message
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        })
+    }
+    // Strange errors (Programming/Bug) => Hide for client
+    else {
+        console.error("ERROR:", err)
+
+        res.status(500).json({
+            status: "error",
+            message: "Something went very wrong!",
+        })
+    }
+}
+
+// GLOBAL ERROR HANDLER MIDDLEWARE
+export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    // Default if no status code input
+    err.statusCode = err.statusCode || 500
+    err.status = err.status || "error"
+
+    if (process.env.NODE_ENV === "development") {
+        sendErrorDev(err, res)
+    } else {
+        // Copy object err để xử lý cho production
+        // Lưu ý: 1 số property của Error không copy được bằng spread operator {...err}
+        // nên gán đè message và name nếu cần thiết
+        sendErrorClient(err, res)
+    }
+}
