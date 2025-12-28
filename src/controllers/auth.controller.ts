c079a3c6f0274c9fc77a0d128ab5fb08bd16379d
ldanh270 <ducanhle.dn@gmail.com>
2025-12-28 16:04:13 +0700

    docs(auth/controller): add note for all authController

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,5 @@
 import { REFRESH_TOKEN_TTL } from "#/configs/constants/authTokens"
+import { HttpStatusCode } from "#/configs/constants/httpStatusCode"
 import { AuthService } from "#/services/auth.service"
 
 import { Request, Response } from "express"
@@ -6,61 +7,96 @@ import { Request, Response } from "express"
 export class AuthController {
     constructor(private readonly service: AuthService) {}
 
+    /**
+     * Sign up controller
+     * @param req Input data to sign up (username, password, email, displayName) (res.body)
+     * @param res Created user data (res.body)
+     */
     signup = async (req: Request, res: Response) => {
-        const { username, password, email, displayName } = req.body
+        try {
+            const { username, password, email, displayName } = req.body
 
-        // Create new user in database
-        await this.service.signup({ username, password, email, displayName })
+            // Create new user in database
+            const user = await this.service.signup({ username, password, email, displayName })
 
-        return res.status(201).json({ message: "User created successfully" })
-    }
-
-    login = async (req: Request, res: Response) => {
-        const { username, password } = req.body
-
-        // Verify logged in user
-        const tokens = await this.service.login({ username, password })
-
-        // Response refresh token (in cookie)
-        res.cookie("refreshToken", tokens.refreshToken, {
-            httpOnly: true,
-            secure: true,
-            sameSite: "none", // For stateless front-end & back-end
-            maxAge: REFRESH_TOKEN_TTL,
-        })
-
-        // Response access token (in res.body)
-        return res.status(200).json({
-            message: `Login successfully`,
-            accessToken: tokens.accessToken,
-        })
-    }
-
-    logout = async (req: Request, res: Response) => {
-        const refreshToken = req.cookies?.refreshToken
-
-        // Delete refresh token
-        if (refreshToken) {
-            this.service.logout(refreshToken)
+            return res.status(HttpStatusCode.CREATED)
+        } catch (error) {
+            console.error("AuthController - signup error:" + error)
+            res.status(HttpStatusCode.INTERNAL_SERVER).json({ message: "Internal server error" })
         }
-
-        // Delete cookie
-        res.clearCookie("refreshtoken")
-
-        return res.status(204)
     }
 
+    /**
+     * Login controller
+     * @param req Input username & password (res.body)
+     * @param res Access token (res.body) & Refresh token (res.cookies)
+     */
+    login = async (req: Request, res: Response) => {
+        try {
+            const { username, password } = req.body
+
+            // Verify logged in user
+            const tokens = await this.service.login({ username, password })
+
+            // Response refresh token (in cookie)
+            res.cookie("refreshToken", tokens.refreshToken, {
+                httpOnly: true,
+                secure: true,
+                sameSite: "none", // For stateless front-end & back-end
+                maxAge: REFRESH_TOKEN_TTL,
+            })
+
+            // Response access token (in res.body)
+            return res.status(HttpStatusCode.OK).json({
+                message: `Login successfully`,
+                accessToken: tokens.accessToken,
+            })
+        } catch (error) {
+            console.error("AuthController - login error:" + error)
+            res.status(HttpStatusCode.INTERNAL_SERVER).json({ message: "Internal server error" })
+        }
+    }
+
+    /**
+     * Logout controller
+     * @param req Existing refresh token (res.cookies)
+     * @param res No content
+     */
+    logout = async (req: Request, res: Response) => {
+        try {
+            const refreshToken = req.cookies?.refreshToken
+
+            // Delete refresh token
+            if (refreshToken) {
+                this.service.logout(refreshToken)
+            }
+
+            // Delete cookie
+            res.clearCookie("refreshtoken")
+
+            return res.status(HttpStatusCode.NO_CONTENT)
+        } catch (error) {
+            console.error("AuthController - logout error:" + error)
+            res.status(HttpStatusCode.INTERNAL_SERVER).json({ message: "Internal server error" })
+        }
+    }
+
+    /**
+     * Get new Access token from Refresh token
+     * @param req Refresh token (res.cookies)
+     * @param res New access token (res.body)
+     */
     getNewToken = async (req: Request, res: Response) => {
         const refreshToken = req.cookies?.refreshToken
 
         // Validate
         if (!refreshToken) {
-            return res.status(401).json({ message: "Token not exists" })
+            return res.status(HttpStatusCode.UNAUTHORIZED).json({ message: "Token not exists" })
         }
 
         // Get new Access token
         const accessToken = await this.service.getNewToken(refreshToken)
 
-        return res.status(200).json({ accessToken })
+        return res.status(HttpStatusCode.OK).json({ accessToken })
     }
 }
